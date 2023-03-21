0f43b1411c201236aad5191260e87ffc7f0ef893
RizkyZaki <ifsry416@gmail.com>
2023-03-21 17:26:16 +0700

    :bento: adding Media Queries

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <VHeader />
-  <router-view />
+  <div class="content">
+    <router-view />
+  </div>
   <VFooter />
 </template>
 
@@ -22,12 +24,19 @@ $text-bg-light: #301e67;
 $dark-bg: #0f172a;
 $light-bg: #ecf2ff;
 
+* {
+  font-size: 22px;
+}
+
 #app {
   font-family: "Lexend";
   background: $light-bg;
   color: $text-bg-light;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+.content {
+  margin: 35px 230px;
 }
 
 .text-base {
@@ -39,5 +48,25 @@ router-link {
 
 .router-link-exact-active.router-link-active {
   color: $text-bg-light;
+}
+
+@media (min-width: 367px) and (max-width: 767px) {
+  * {
+    font-size: 1rem;
+  }
+
+  .content {
+    margin: 35px 1.5rem;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+  * {
+    font-size: 1.1rem;
+  }
+
+  .content {
+    margin: 35px 1.5rem;
+  }
 }
 </style>
