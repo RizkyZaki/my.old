a8fbe1367387616253af3dd4f4f5e6ff239d6b1a
RizkyZaki <ifsry416@gmail.com>
2023-03-27 20:42:35 +0700

    :boom: Project: Fixing, Clear project page

--- src/Pages/ProjectPage.vue
+++ src/Pages/ProjectPage.vue
@@ -4,46 +4,70 @@
     <div class="row">
       <div class="col-md-4 mb-3">
         <div class="card-project">
-          <img src="" alt="" />
+          <img src="../assets/img/news.png" alt="" />
           <div class="container">
-            <h6>Name Project</h6>
-            <p>Desc</p>
+            <h6 class="mt-3">Tenews - News Website</h6>
+            <p class="mt-4 mb-5">
+              news website created using codeigniter, and there is admin
+              management
+            </p>
+            <a href=""><i class="fa-brands fa-square-github icon-link"></i></a>
           </div>
         </div>
       </div>
       <div class="col-md-4 mb-3">
         <div class="card-project">
-          <img src="" alt="" />
+          <img src="../assets/img/spp.png" alt="" />
           <div class="container">
-            <h6>Name Project</h6>
-            <p>Desc</p>
+            <h6 class="mt-3">E-SPP - School Payments</h6>
+            <p class="mt-4 mb-5">
+              website that facilitates the school in administrative payments
+            </p>
+            <a href=""><i class="fa-brands fa-square-github icon-link"></i></a>
           </div>
         </div>
       </div>
       <div class="col-md-4 mb-3">
         <div class="card-project">
-          <img src="" alt="" />
+          <img src="../assets/img/mamaD.png" alt="" />
           <div class="container">
-            <h6>Name Project</h6>
-            <p>Desc</p>
+            <h6 class="mt-3">Mama D Cookies - E-Commerce Cake</h6>
+            <p class="mt-4 mb-5">
+              digital cake sales website with VUE & REST API Laravel
+            </p>
+            <a href=""><i class="fa-brands fa-square-github icon-link"></i></a>
           </div>
         </div>
       </div>
       <div class="col-md-4 mb-3">
         <div class="card-project">
-          <img src="" alt="" />
+          <img src="../assets/img/inv.png" alt="" />
           <div class="container">
-            <h6>Name Project</h6>
-            <p>Desc</p>
+            <h6 class="mt-3">Inventory Admin</h6>
+            <p class="mt-4 mb-5">
+              website to manage bookkeeping, stock, expenditure of an item
+            </p>
           </div>
         </div>
       </div>
       <div class="col-md-4 mb-3">
         <div class="card-project">
-          <img src="" alt="" />
+          <img src="../assets/img/e-learn.png" alt="" />
           <div class="container">
-            <h6>Name Project</h6>
-            <p>Desc</p>
+            <h6 class="mt-3">School APP</h6>
+            <p class="mt-4 mb-5">
+              a project to manage existing learning materials in schools
+            </p>
+            <a href=""><i class="fa-brands fa-square-github icon-link"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card-project">
+          <img src="../assets/img/book.png" alt="" />
+          <div class="container">
+            <h6 class="mt-3">My BOOK</h6>
+            <p class="mt-4 mb-5">book e-commerce with PHP Native</p>
           </div>
         </div>
       </div>
@@ -64,6 +88,9 @@
   img {
     width: 100%;
     height: auto;
+  }
+  .icon-link {
+    font-size: 27px;
   }
 }
 
