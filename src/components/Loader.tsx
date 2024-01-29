cb29b0af144019dc74807153a704769838bf8f98
RizkyZaki <ifsry416@gmail.com>
2024-01-29 10:36:28 +0700

    :sparkles: FIx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Logo from "./Logo";
 import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
 
 function Loader({ isLoading, setIsLoading }: any) {
   useEffect(() => {
@@ -20,52 +21,21 @@ function Loader({ isLoading, setIsLoading }: any) {
             ease: "easeInOut",
           }}
         >
-          <motion.svg
-            id="logo"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-          >
-            <title>Logo</title>
-            <g>
-              <g id="Z" transform="translate(35.000000, 35.000000)">
-                <motion.path
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{
-                    duration: 1,
-                    ease: "easeInOut",
-                    delay: 1,
-                  }}
-                  exit={{
-                    scale: 2,
-                  }}
-                  fill="currentColor"
-                  d="M 20,30 L 10,10 L 20,0 C 21,1, 22,2, 23,3 L 25,7 C 24,6, 23,5, 22,4 L 20,8 L 18,4 C 17,5, 16,6, 15,7 L 17,3 L 20,0 L 22,4 C 23,5, 24,6, 25,7 L 27,3 C 26,2, 25,1, 24,0 L 20,0 L 30,20 L 20,40 L 10,20 L 20,0 Z "
-                />
-              </g>
-              <motion.path
-                initial={{ pathLength: 0 }}
-                animate={{ pathLength: 1 }}
-                transition={{
-                  duration: 1,
-                  ease: "easeInOut",
-                }}
-                exit={{
-                  scale: 2,
-                }}
-                stroke="currentColor"
-                strokeWidth="5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M 50, 5
-          L 11, 27
-          L 11, 72
-          L 50, 95
-          L 89, 73
-          L 89, 28 z"
-              />
-            </g>
-          </motion.svg>
+          <title>Logo</title>
+          <motion.path
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 1,
+              ease: "easeInOut",
+              delay: 1,
+            }}
+            exit={{
+              scale: 2,
+            }}
+            fill="currentColor"
+          />
+          <Image src="/logo.png" alt="logo" width={100} height={100} />
         </motion.div>
       )}
     </AnimatePresence>
